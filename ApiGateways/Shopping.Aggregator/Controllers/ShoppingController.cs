f58e2a78f788e87386be871960cca94c75fe2bc0
ruwanweerasena <ruwan.weerasena2001@gmail.com>
2023-10-17 11:33:33 +0530

    complete aggregator

---
+++ ApiGateways/Shopping.Aggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services;
+using System.Net;
+
+namespace Shopping.Aggregator.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class ShoppingController : ControllerBase
+{
+    private readonly ICatalogService _catalogService;
+    private readonly IBasketService _basketService;
+    private readonly IOrderService _orderService;
+
+    public ShoppingController(IOrderService orderService, IBasketService basketService, ICatalogService catalogService)
+    {
+        _orderService = orderService;
+        _basketService = basketService;
+        _catalogService = catalogService;
+    }
+
+    [HttpGet("{userName}",Name = "GetShopping")]
+    [ProducesResponseType(typeof(ShoppingModel),(int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ShoppingModel>> GetShopping(string userName)
+    {
+        //getbasket with username
+        var basket = await _basketService.GetBasket(userName);
+        //iterate items and populate other fields using item productId
+        foreach (var basketItem in basket.Items)
+        {
+            var product = await _catalogService.GetCatalog(basketItem.ProductId);
+            //set additional fields
+            basketItem.Category =product.Category;
+            basketItem.Summery= product.Summery;
+            basketItem.Description= product.Description;
+            basketItem.ImageFile= product.ImageFile;
+        }
+        //consume ordering ms and get orders list
+        var orders = await _orderService.GetOrdersByUserName(userName);
+        //return root shoppingmodel dto class which including all responses
+        var shoppingModel = new ShoppingModel
+        {
+            UserName = userName,
+            BasketWithProducts = basket,
+            Orders = orders
+        };
+
+        return Ok(shoppingModel);
+        
+    }
+
+}
---
+++ ApiGateways/Shopping.Aggregator/Models/BasketItemExtendedModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Shopping.Aggregator.Models;
+
+public class BasketItemExtendedModel
+{
+    public int Quantity { get; set; }
+    public string Color { get; set; }
+    public decimal Price { get; set; }
+    public string ProductId { get; set; }
+    public string ProductName { get; set; }
+
+    //extended ones
+    public string Category { get; set; }
+    public string Summery { get; set; }
+    public string Description { get; set; }
+    public string ImageFile { get; set; }
+}
